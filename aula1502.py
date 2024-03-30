48022bb2e6a66d452cf1148987f6294be0a76da3
Lívia Bettero <121136618+lbettero@users.noreply.github.com>
2024-03-30 14:34:58 +0100

    Add files via upload

--- aula1502.py
+++ aula1502.py
@@ -4,17 +4,20 @@ from grava_resultados import *
 
 def aula1502():
     ats= glob("www.nos.uminho.pt/Article.aspx*")
-    print (ats)
+    #print (ats)
 
     def proc_article(html):
         a=bs(html) # cria uma árvore documental
-        print(a)
+        #print(a)
         art= a.find("div", id="artigo") #procura no html
-        texto=(art.get_text())
-        print (texto)
-        arquivo_origem = file.split(".")
-        arquivo_destino = arquivo_origem[3][3:]+"-"+arquivo_origem[-1][-4:]
-        grava_resultado(texto,arquivo_destino,"aula1502")
+        try:
+            texto=(art.get_text())
+            #print (texto)
+            arquivo_origem = file.split(".")
+            arquivo_destino = arquivo_origem[3][3:]+"-"+arquivo_origem[-1][-4:]
+            grava_resultado(texto,arquivo_destino,"aula1502")
+        except Exception as e:
+            pass
 
     for file in ats:
         with open(file, encoding="utf-8") as f:
--- aula1602.py
+++ aula1602.py
@@ -19,6 +19,22 @@ def aula1602():
                 continue
             atributo = atributo.replace("og:","") #para remover a o pedaço de texto indesejado
             cabecalho+= f"{atributo}: {meta.get('content')}\n"
+        #Outro modo de extrair a data e o nome do autor da entrevista / matéria
+        #buscando diretamente no HTML em vez do txt extraído.
+        try:
+            obter_data=a.find("span", id="ctl00_ContentPlaceHolder1_LabelInfo").text #obter o span de id ctl00_ContentPlaceHolder1_LabelInfo
+            extrair_data=obter_data[:10]#obter os dez primeiros caracteres, correspondentes à data
+            cabecalho+= f"data: {extrair_data}\n"#coloca a data no cabeçalho
+            autor=obter_data.split(" | ") #aproveita a ocorrência do nome do autor logo após a data
+            obter_autor=autor[-1][:] #retira apenas o nome do autor da string original
+            cabecalho+= f"autor: {obter_autor}\n"#adiciona o nome do autor ao cabeçalho
+        except Exception as e:
+            pass
+        cabecalho+=f"---\n"
+
+
+
+
         arquivo_origem = file.split(".")
         arquivo_destino = arquivo_origem[3][3:]+"-"+arquivo_origem[-1][-4:]
         grava_resultado(cabecalho,arquivo_destino,"aula1602")
@@ -28,6 +44,10 @@ def aula1602():
             html= f.read() 
         busca_meta_dados(html)
 
+#ECERCÍCIO 1: PROCURAR AS DATAS, COLOCANDO-AS NO CABEÇALHO
+#Obs. Adicionalmente foram encontrados os nomes dos autores das entrevistas/entrevistadores,
+#que também foram colocados no cabeçalho
+
 def aula1602_b():
     fonte_cabecalho="aula1602"
     fonte_artigo="aula1502"
@@ -36,16 +56,20 @@ def aula1602_b():
         with open (fonte_cabecalho+"/"+arquivo, encoding="utf-8") as cabecalho_in:
             texto=cabecalho_in.read()
             texto_completo=texto.splitlines()
-            with open (fonte_artigo+"/"+arquivo, encoding="utf-8") as artigo_in:
-                texto=artigo_in.read()
-                artigo=texto.splitlines()
-                for linha in artigo:
-                    if "|" in linha:
-                        autor=linha.split(" | ")
-                        texto_completo.append("data: "+autor[0][:])
-                        texto_completo.append("autor: "+autor[-1][:])
-                texto_completo.append("---")
-                for linha in artigo:
-                    if linha != "" and "voltar à página anterior" not in linha and len(linha)>3:
-                        texto_completo.append(linha)
+            try:
+                with open (fonte_artigo+"/"+arquivo, encoding="utf-8") as artigo_in:
+                    texto=artigo_in.read()
+                    artigo=texto.splitlines()
+                    #BLOCO DESATIVADO AO ADICIONAR A VERSÃO FEITA NA AULA DE 22 DE FEVEREIRO
+                    #for linha in artigo:
+                    #    if "|" in linha:
+                    #        autor=linha.split(" | ")
+                    #        texto_completo.append("data: "+autor[0][:])
+                    #        texto_completo.append("autor: "+autor[-1][:])
+                    #texto_completo.append("---")
+                    for linha in artigo:
+                        if linha != "" and "voltar à página anterior" not in linha and len(linha)>3:
+                            texto_completo.append(linha)
+            except Exception as e:
+                pass
         grava_resultado(texto_completo,arquivo,"aula1602_b")
